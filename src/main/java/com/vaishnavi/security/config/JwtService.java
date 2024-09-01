c090e37c7eb1ec07fdbc2bbfafe1c251ee547a1a
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-09-01 12:02:01 +0530

    Add validate token logic in Jwt Service

--- src/main/java/com/vaishnavi/security/config/JwtService.java
+++ src/main/java/com/vaishnavi/security/config/JwtService.java
@@ -2,7 +2,6 @@ package com.vaishnavi.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -43,6 +42,19 @@ public class JwtService {
                 .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
                 .signWith(getSignInKey(), Jwts.SIG.HS256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     private Claims extractAllClaims(String token) {
