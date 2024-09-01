776a0571eefeb53dd0006c9f73894d233128bea2
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-09-01 19:11:32 +0530

    Add controller layer implementation for auth APIs like register and login

--- src/main/java/com/vaishnavi/security/auth/AuthenticationService.java
+++ src/main/java/com/vaishnavi/security/auth/AuthenticationService.java
@@ -6,4 +6,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        return null;
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        return null;
+    }
 }
