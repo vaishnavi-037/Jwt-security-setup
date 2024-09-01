d2a993c512d1506daa568406bede0a46d09c2a26
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-09-01 19:16:49 +0530

    Implement register service layer to register the user in the application

--- src/main/java/com/vaishnavi/security/auth/AuthenticationService.java
+++ src/main/java/com/vaishnavi/security/auth/AuthenticationService.java
@@ -1,14 +1,37 @@
 package com.vaishnavi.security.auth;
 
+import com.vaishnavi.security.config.JwtService;
+import com.vaishnavi.security.user.Role;
+import com.vaishnavi.security.user.User;
+import com.vaishnavi.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        User user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        repository.save(user);
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
