38b6ccab87d71be33d72697edf6141cc71a6795b
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-09-01 18:32:51 +0530

    Add implementation of AuthenticationProvider used in spring security filter chain

--- src/main/java/com/vaishnavi/security/config/ApplicationConfig.java
+++ src/main/java/com/vaishnavi/security/config/ApplicationConfig.java
@@ -4,8 +4,12 @@ import com.vaishnavi.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -29,4 +33,18 @@ public class ApplicationConfig {
 //            }
 //        };
 //    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
