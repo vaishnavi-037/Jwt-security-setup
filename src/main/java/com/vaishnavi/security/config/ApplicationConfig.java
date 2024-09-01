fd9e44ba72dbd3b9662eef7621bfe4965fefe439
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-09-01 18:59:18 +0530

    Add implementation of AuthenticationManager for user authentication based on username and password

--- src/main/java/com/vaishnavi/security/config/ApplicationConfig.java
+++ src/main/java/com/vaishnavi/security/config/ApplicationConfig.java
@@ -4,8 +4,10 @@ import com.vaishnavi.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,6 +43,11 @@ public class ApplicationConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
