b82fce5681d4e357b5803445c2839eefc423c784
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-09-01 19:09:12 +0530

    Add controller layer for auth APIs like register and login

---
+++ src/main/java/com/vaishnavi/security/auth/AuthenticationController.java
@@ -0,0 +1,31 @@
+package com.vaishnavi.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ){
+
+
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request
+    ){
+
+    }
+
+
+}
---
+++ src/main/java/com/vaishnavi/security/auth/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.vaishnavi.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+}
